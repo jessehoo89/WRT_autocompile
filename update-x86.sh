0f238ac03a6444105d48ef92917d87d36bfd3cad
jessehoo89 <66499464+jessehoo89@users.noreply.github.com>
2025-08-18 16:03:37 +0800

    Update update-x86.sh

--- update-x86.sh
+++ update-x86.sh
@@ -914,7 +914,7 @@ main() {
     update_uwsgi_limit_as
     update_argon
     install_feeds
-    apply_hash_fixes # 调用哈希修正函数
+    # apply_hash_fixes # 调用哈希修正函数
     support_fw4_adg
     update_script_priority
     fix_easytier
